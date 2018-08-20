f614b27372bb99b79606f0a97d25fa25e1dd47dc
Paul Guilbert <guilbert.paul@gmail.com>
2018-08-20 23:02:55 +0200

    Remove the use of Blazor in the client app. (Still in use for hosting, publishing and debugging)

---
+++ GuidGenerator/wwwroot/generator.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    initMono(['_framework/_bin/GuidGeneratorClient.dll'], function () {
+        el('loadingIndicator').style.display = 'none';
+        el('generator').style.display = 'block';
+    });
+
+    el('btn-action-new-guid').onclick = function (e) {
+        e.preventDefault();
+        var result = invokeMonoMethod('GuidGeneratorClient', 'GuidGeneratorClient', 'GuidHelper', 'NewGuid', []);
+        el('guid-placeholder').innerText = result;
+    };
+
+    function el(id) {
+        return document.getElementById(id);
+    }
+})();
---
+++ GuidGeneratorClient/GuidHelper.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace GuidGeneratorClient
+{
+    public static class GuidHelper
+    {
+        public static string NewGuid() => Guid.NewGuid().ToString();
+    }
+}

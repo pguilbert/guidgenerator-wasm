f533613fa419efb56ea9fd47ca0ad832ca5e43f2
Paul Guilbert <guilbert.paul@gmail.com>
2018-08-22 13:44:15 +0200

    Update gh pages with the new version without blazor.

---
+++ generator.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    initMono(["_framework/_bin/GuidGeneratorClient.dll"], function () {
+        el("loadingIndicator").style.display = "none";
+        el("generator").style.display = "block";
+
+        generateAndShowNewGuid(); // Generate one by default.
+    });
+
+    el("btn-action-new-guid").onclick = function (e) {
+        e.preventDefault();
+        generateAndShowNewGuid();
+    };
+
+    function generateAndShowNewGuid() {
+        var result = invokeMonoMethod("GuidGeneratorClient", "GuidGeneratorClient", "GuidHelper", "NewGuid", []);
+        el("guid-placeholder").innerText = result;
+    }
+
+    function el(id) {
+        return document.getElementById(id);
+    }
+})();
